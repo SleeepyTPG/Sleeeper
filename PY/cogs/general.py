29f82cac4b9bdfcd39682320a5dc738a0b8d902f
SleeepyTPG <itstheonlyway@outlook.de>
2025-04-25 23:31:15 +0200

    + Big Update concluded

--- PY/cogs/general.py
+++ PY/cogs/general.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+BOT_VERSION = "0.5.0 Beta Build"
+NEXT_VERSION = "0.5.1 Beta Build"
+
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,16 +17,18 @@ class General(commands.Cog):
     @app_commands.command(name="info", description="Shows information about the bot")
     async def info(self, interaction: discord.Interaction):
         embed = discord.Embed(title="Bot Info", color=discord.Color.blue())
-        embed.add_field(name="Version", value="0.4.2 Beta Build", inline=False)
+        embed.add_field(name="Version", value=BOT_VERSION, inline=False)
+        embed.add_field(name="Helper", value="<@1296122732173590651>", inline=False)
         embed.add_field(name="Coder", value="<@1104736921474834493>", inline=False)
+        embed.add_field(name="Support Server", value="https://discord.gg/WwApdk4z4H", inline=False)
         await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="version", description="Shows the current version")
     async def version_info(self, interaction: discord.Interaction):
         embed = discord.Embed(title="Version Info", color=discord.Color.blue())
         embed.add_field(name="Bot Name", value=self.bot.user.name, inline=False)
-        embed.add_field(name="Version", value="0.4.2 Beta Build", inline=False)
-        embed.add_field(name="Next Version", value="0.4.3 Beta Build", inline=False)
+        embed.add_field(name="Version", value=BOT_VERSION, inline=False)
+        embed.add_field(name="Next Version", value=NEXT_VERSION, inline=False)
         embed.add_field(name="Release Date", value="TBA", inline=False)
         embed.add_field(name="Support Server", value="https://discord.gg/WwApdk4z4H", inline=False)
         embed.add_field(name="Extra Info", value="**Note:** This bot is in Beta phase.", inline=False)
